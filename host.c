2777d379ca5e281f4f8c8caf8ed2791889aa41e3
elenambarbour <ebarbour@hawaii.edu>
2017-05-10 17:14:10 -1000

    Added empty server file

--- host.c
+++ host.c
@@ -22,6 +22,7 @@
 #define MAX_MSG_LENGTH 100
 #define MAX_DIR_NAME 100
 #define MAX_FILE_NAME 100
+#define MAX_DOMAIN_NAME 50
 #define PKT_PAYLOAD_MAX 100
 #define TENMILLISEC 10000   /* 10 millisecond sleep */
 
---
+++ server.h
@@ -0,0 +1,10 @@
+//server.h
+
+
+struct switch_table_format {
+	int valid_status;	
+	char dest;	
+	int port_num;	
+};
+
+void switch_main(int switch_id);
